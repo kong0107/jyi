99386bf85606335a9c4fb6c820d301fd6b98f4c9
kong0107 <kong0107@gmail.com>
2018-11-29 20:02:15 +0800

    create download.js and update raw data to 769

---
+++ download.js
@@ -0,0 +1,30 @@
+const https = require("https");
+const fs = require("fs");
+
+const download = (url, filename) => {
+    const ws = fs.createWriteStream(filename);
+    return new Promise((resolve, reject) =>
+        https.get(url, rs => {
+            rs.pipe(ws);
+            rs.on("end", resolve);
+            rs.on("error", reject)
+        })
+    )
+};
+
+const queue = [];
+for(let number = 725; number <= 729; ++number) {
+    queue.push(() => {
+        const url = (number <= 736)
+            ? `https://www.judicial.gov.tw/constitutionalcourt/p03_01.asp?expno=${number}`
+            : `https://www.judicial.gov.tw/constitutionalcourt/p03_01_1.asp?expno=${number}`
+        ;
+        console.log(number);
+        return download(url, `./downloads/${number}.html`);
+    });
+    
+    // 刻意等待
+    queue.push(() => new Promise(resolve => setTimeout(resolve, 1000 + 1000 * Math.random())));
+}
+
+queue.reduce((acc, cur) => acc.then(cur), Promise.resolve());
