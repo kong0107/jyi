5a6721f67d61899155948eec670e153f6430d929
kong0107 <kong0107@gmail.com>
2018-12-14 21:05:13 +0800

    add jyi 771

--- download.js
+++ download.js
@@ -14,7 +14,7 @@ const download = (url, filename) => {
 };
 
 const queue = [];
-for(let number = 770; number <= 770; ++number) {
+for(let number = 771; number <= 771; ++number) {
     queue.push(() => {
         const url = (number <= 736)
             ? `https://www.judicial.gov.tw/constitutionalcourt/p03_01.asp?expno=${number}`
