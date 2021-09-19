a1f7c20077b680e0155ba3edd04bfb42dd922e1e
kong0107 <kong0107@gmail.com>
2021-09-19 21:45:31 +0800

    update `download.js` to auto-detect whether a JYI exists

--- download.js
+++ download.js
@@ -2,27 +2,53 @@
 const https = require("https");
 const fs = require("fs");
 
-const download = (url, filename) => {
-    const ws = fs.createWriteStream(filename);
-    return new Promise((resolve, reject) =>
-        https.get(url, rs => {
-            rs.pipe(ws);
-            rs.on("end", resolve);
-            rs.on("error", reject)
-        })
-    )
-};
-
-const queue = [];
-for(let number = 1; number <= 781; ++number) {
-    queue.push(() => {
-        const url = `https://cons.judicial.gov.tw/jcc/zh-tw/jep03/show?expno=${number}`;
-        console.log(number);
-        return download(url, `./downloads/${number}.html`);
+function fileExists(filepath, mode = fs.constants.F_OK) {
+    return new Promise(resolve => {
+        fs.access(filepath, mode, err => resolve(!err));
     });
-
-    // 刻意等待
-    queue.push(() => new Promise(resolve => setTimeout(resolve, 1000 + 1000 * Math.random())));
 }
 
-queue.reduce((acc, cur) => acc.then(cur), Promise.resolve());
+async function download(url, filepath) {
+    return new Promise((resolve, reject) => {
+        const onError = err => {
+            fs.unlink(filepath, delErr => {
+                if(delErr) console.error(delErr);
+                reject(delErr || err);
+            });
+        }
+
+        const ws = fs.createWriteStream(filepath);
+        ws.on('close', resolve);
+        ws.on('error', onError);
+
+        const request = https.get(url, res => {
+            if(res.statusCode !== 200) return onError(res.statusMessage);
+            res.pipe(ws);
+        });
+        request.on('error', onError);
+        request.end();
+    });
+}
+
+async function main() {
+    for(let number = 1; ; ++number) {
+        const filepath = `./downloads/${number}.html`;
+        if(await fileExists(filepath, fs.constants.R_OK)) {
+            console.log(`jyi#${number} had been downloaded before.`);
+            continue;
+        }
+        try {
+            await download(
+                `https://cons.judicial.gov.tw/jcc/zh-tw/jep03/show?expno=${number}`,
+                filepath
+            );
+            console.log(`jyi#${number} has been downloaded just now.`);
+        }
+        catch {
+            console.log(`jyi#${number} does not exist yet.`);
+            break;
+        }
+    }
+}
+
+main();
